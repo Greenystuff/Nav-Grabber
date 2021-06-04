ab0177d725eb49112e2d9481d6755ca6604d1f4c
Greenystuff <david_meunier@hotmail.fr>
2021-06-04 10:53:20 +0200

    v1.0

--- Extension Chrome/StreamDeck Chrome/background.js
+++ Extension Chrome/StreamDeck Chrome/background.js
@@ -2,6 +2,11 @@ var ws = new WebSocket('ws://localhost:8088/post');
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete") {
-    ws.send(tab.url);
+		var json = {
+    	"url": tab.url,
+    	"Favicon": tab.favIconUrl	
+    };
+    console.log("payload : " + JSON.stringify(json));
+		ws.send(JSON.stringify(json));
   }
 });
--- SocketStreamDeck/server.js
+++ SocketStreamDeck/server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 const url = require('url');
-let msg = '';
+let payload = null;
 const wss = new WebSocket.Server({ port: 8088 });
 
 wss.on('connection', function connection(ws, request) {
@@ -9,12 +9,12 @@ wss.on('connection', function connection(ws, request) {
   if (pathname === '/post') {
     ws.on('message', function incoming(message) {
       console.log('received: %s', message);
-      msg = message;
+      payload = message;
     });
   }
 
   if (pathname === '/get') {
-      console.log('sending : %s', msg);
-      ws.send(msg);
+    console.log('sending : %s', payload);   
+      ws.send(payload);
   }
 });
